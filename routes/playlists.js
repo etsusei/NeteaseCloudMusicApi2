366a7bdbb75c2078b345154bde7514d901ed991f
etsusei <exclusiveyao@gmail.com>
2025-12-11 00:13:32 +0900

    Update playlists.js

--- routes/playlists.js
+++ routes/playlists.js
@@ -83,7 +83,7 @@ router.get('/:id/songs', async (req, res) => {
     }
 
     const result = await pool.query(
-      'SELECT * FROM playlist_songs WHERE playlist_id = $1 ORDER BY added_at ASC',
+      'SELECT * FROM playlist_songs WHERE playlist_id = $1 ORDER BY added_at DESC',
       [id]
     );
     res.json({ code: 200, data: result.rows });
